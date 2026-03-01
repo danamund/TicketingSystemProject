9bab61c06c8858f0bb9fe7355e4bb1f4af81be98
lorenkr <lorenkr20@gmail.com>
2026-03-01 15:06:57 +0200

    הפרדה בין חלק א לחלק ב

--- TicketBookingSystem/src/main/java/com/hit/dao/IDao.java
+++
@@ -1,9 +0,0 @@
-package com.hit.dao;
-import java.util.List;
-
-public interface IDao<T> {
-    void saveAll(List<T> entities);
-    List<T> getAll();
-    boolean save(T entity);
-    boolean delete(T entity);
-}
--- TicketBookingSystem/src/main/java/com/hit/server/ServerDriver.java
+++
@@ -1,8 +0,0 @@
-package com.hit.server;
-
-public class ServerDriver {
-    public static void main(String[] args) {
-        Server server = new Server(34567);
-        new Thread(server).start();
-    }
-}
